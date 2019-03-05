f223f4c00e5cc6311367d1d8d8f1fe15027a186c
Thomas Deal <justarandomgeek@gmail.com>
2019-03-05 14:05:54 -0500

    Common tech with Optera

--- data.lua
+++ data.lua
@@ -25,14 +25,40 @@ end
 magic_lamp_item = copyPrototype("item","constant-combinator","magic-lamp")
 magic_lamp_entity = copyPrototype("constant-combinator","constant-combinator","magic-lamp")
 
-if data.raw.technology["circuit-network"] then
-  if not data.raw.technology["circuit-network"].effects then data.raw.technology["circuit-network"].effects = {} end
-
-  table.insert(data.raw.technology["circuit-network"].effects,
+-- Common tech with Utility Combinators and Optera's Inventory Sensor
+if data.raw["technology"]["circuit-network-2"] then
+  table.insert( data.raw["technology"]["circuit-network"].effects,
     {
-      type = "unlock-recipe",
-      recipe = "magic-lamp"
-    })
+        type = "unlock-recipe",
+        recipe = "magic-lamp"
+    } )
+else
+  data:extend({
+    {
+      type = "technology",
+      name = "circuit-network-2",
+      icon = "__base__/graphics/technology/circuit-network.png",
+      icon_size = 128,
+      prerequisites = {"circuit-network", "advanced-electronics"},
+      effects =
+      {
+        {
+          type = "unlock-recipe",
+          recipe = "magic-lamp"
+        }
+      },
+      unit =
+      {
+        count = 150,
+        ingredients = {
+          {"automation-science-pack", 1},
+          {"logistic-science-pack", 1},
+        },
+        time = 30
+      },
+      order = "a-d-d"
+    }
+  })
 end
 
 data:extend({
